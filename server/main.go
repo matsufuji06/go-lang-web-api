a41d9435a26d94e093041eabc71f67ba3b7b9804
matsufuji06 <n.matsufuji06@gmail.com>
2025-12-26 20:22:50 +0900

    fixed: main.goで静的ファイルをハンドラ設定

--- server/main.go
+++ server/main.go
@@ -14,6 +14,20 @@ func main() {
 		log.Println("ERROR loading .env:", err)
 	}
 
+	// 静的ファイル(JSファイル)の配信設定
+	http.Handle(
+		"/static/",
+		http.StripPrefix(
+			"/static/",
+			http.FileServer(http.Dir("./static")),
+		),
+	)
+
+	// index.htmlの配信設定
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+	})
+
 	http.HandleFunc("/api/v1/videos", handler.GetVideos)
 	http.ListenAndServe(":8000", nil)
 
