e69a7634f40c7c0ba6ddeb44a48d98c7b847c83e
misaki <mwtj96762@gmail.com>
2025-12-27 00:28:41 +0900

    Merge branch 'develop' of https://github.com/matsufuji06/go-lang-web-api into feature/trending-genre-analysis

--- server/main.go
+++ server/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"net/http"
+	handler "go-lang-web-api/server/handler"
+)
+func main(){
+	http.HandleFunc("/api/v1/analytics/genres", handler.GetTrends)
+	http.ListenAndServe(":8000", nil)
+}
