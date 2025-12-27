53ab0005fac29573dc94dc394cba0b7ac899c1f8
misaki <mwtj96762@gmail.com>
2025-12-27 13:25:05 +0900

    refactor: API専用にレートリミットを適用

--- server/main.go
+++ server/main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
-	"net/http"
 	handler "go-lang-web-api/server/handler"
+	"net/http"
+	"golang.org/x/time/rate"
+		util "go-lang-web-api/server/service/util"
 )
-func main(){
-	http.HandleFunc("/api/v1/analytics/genres", handler.GetTrends)
-	http.ListenAndServe(":8000", nil)
+var limiter = rate.NewLimiter(1, 1) 
+func rateLimitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			util.ResponseErrorJson(w, http.StatusTooManyRequests,"API rate limit exceeded. Please try again later" )
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	// メインの mux（HTML / static 用）
+	mux := http.NewServeMux()
+
+	// 静的ファイル
+	mux.Handle(
+		"/static/",
+		http.StripPrefix(
+			"/static/",
+			http.FileServer(http.Dir("./static")),
+		),
+	)
+
+	// index.html
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "go-lang-web-api/index.html")
+	})
+
+	// --- API 用 mux ---
+	apiMux := http.NewServeMux()
+	apiMux.HandleFunc("/api/v1/analytics/genres", handler.GetTrends)
+
+	// API にだけレート制限を適用
+	mux.Handle("/api/", rateLimitMiddleware(apiMux))
+
+	http.ListenAndServe(":8000", mux)
 }
