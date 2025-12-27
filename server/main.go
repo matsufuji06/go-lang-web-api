7d95922385ec511c0e49b95ab1b0a33c9462fece
Morry <hm452317@gmail.com>
2025-12-27 23:07:26 +0900

    fix: main.goとindex.htmlのコンフリクトを解消

--- server/main.go
+++ server/main.go
@@ -1,34 +1,19 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"fmt"
-	"go-lang-web-api/server/handler"
-	"log"
-	"net/http"
-
-	"github.com/joho/godotenv"
-)
-
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	// 静的ファイル(JSファイル)の配信設定
-	http.Handle(
-=======
 	handler "go-lang-web-api/server/handler"
+	utils "go-lang-web-api/server/service/utils"
 	"net/http"
+
 	"golang.org/x/time/rate"
-		utils "go-lang-web-api/server/service/utils"
 )
-var limiter = rate.NewLimiter(1, 1) 
+
+var limiter = rate.NewLimiter(1, 1)
+
 func rateLimitMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !limiter.Allow() {
-			utils.ResponseErrorJson(w, http.StatusTooManyRequests,"API rate limit exceeded. Please try again later" )
+			utils.ResponseErrorJson(w, http.StatusTooManyRequests, "API rate limit exceeded. Please try again later")
 			return
 		}
 		next.ServeHTTP(w, r)
@@ -41,7 +26,6 @@ func main() {
 
 	// 静的ファイル
 	mux.Handle(
->>>>>>> 23e4720dc6159ebb9ccb7de2dad5870bdfef61d7
 		"/static/",
 		http.StripPrefix(
 			"/static/",
@@ -49,17 +33,6 @@ func main() {
 		),
 	)
 
-<<<<<<< HEAD
-	// index.htmlの配信設定
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./index.html")
-	})
-
-	fmt.Println("Server is running!")
-	http.HandleFunc("/api/v1/channels", handler.GetChannelInfo)
-	http.ListenAndServe(":8000", nil)
-
-=======
 	// index.html
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./index.html")
@@ -68,10 +41,10 @@ func main() {
 	// --- API 用 mux ---
 	apiMux := http.NewServeMux()
 	apiMux.HandleFunc("/api/v1/analytics/genres", handler.GetTrends)
+	apiMux.HandleFunc("/api/v1/channels", handler.GetChannelInfo)
 
 	// API にだけレート制限を適用
 	mux.Handle("/api/", rateLimitMiddleware(apiMux))
 
 	http.ListenAndServe(":8000", mux)
->>>>>>> 23e4720dc6159ebb9ccb7de2dad5870bdfef61d7
 }
